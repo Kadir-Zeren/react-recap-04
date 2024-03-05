18a925e04ea4a2f809b3ea6ab42285ee2f610c8f
kadir-zeren <k4dzeren@gmail.com>
2024-03-05 12:30:41 +0300

    card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import data from "./data";
+
 function App() {
+  console.log(data);
   return (
     <div className="App">
       <h1>Hello React</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
